251b02588dd9a75c07c9249a3136ad87d1e1d12b
Vatsal Kanoria <vatskan@gmail.com>
2021-10-02 01:17:04 +0200

    abstract out grid and column size into game data

--- quaze.js
+++ quaze.js
@@ -41,6 +41,10 @@ const z = 'zgate'
 const T = true
 const F = false
 const gameData = {
+  gridSize: {
+    rows: 5,
+    columns: 5
+  },
   gatesGrid: [[h, x, z, h, x], [z, h, x, x, h], [h, z, h, x, z], [z, x, z, h, h], [x, h, x, z, h]], //each subarray represents a row starting from the top of the grid
   horizontalWalls: [[T, T, T, T, T], [F, T, F, T, F], [F, F, T, T, F], [F, F, F, F, F], [F, F, T, T, T], [T, T, T, T, T]], //each subarray represents a row starting from the top of the grid
   verticalWalls: [[F, T, T, T, T], [F, T, T, T, T], [F, F, F, T, F], [F, F, F, F, F], [F, F, T, T, F], [T, T, T, T, F]] //each subarray represents a column starting from the left of the grid
@@ -48,13 +52,13 @@ const gameData = {
 
 function create ()
 {
-  for(let row=0; row<5; row++){
-    for(let column=0; column<5; column++){
+  for(let row=0; row<gameData.gridSize.rows; row++){
+    for(let column=0; column<gameData.gridSize.columns; column++){
       this.add.image(xCoordinateGrid(row), yCoordianteGrid(column), gameData.gatesGrid[row][column])
     }
   }
-  for(let row=0; row<6; row++){
-    for(let column=0; column<5; column++){
+  for(let row=0; row<gameData.gridSize.rows+1; row++){
+    for(let column=0; column<gameData.gridSize.columns; column++){
       if(gameData.horizontalWalls[row][column]){
         this.add.image(xCoordinateGrid(column), yCoordianteGrid(row)-50, 'wallh'); //TODO: remove magic value '50' by changing asset?
       }
