86f2f5f3dff90a056d8ab0396fe6b373ea12fcd4
Vatsal Kanoria <vatskan@gmail.com>
2021-10-01 19:09:53 +0200

    finish refactor to remove magic values from qubits

--- quaze.js
+++ quaze.js
@@ -1,7 +1,7 @@
 let config = {
   type: Phaser.AUTO,
-  width: 1200,
-  height: 800,
+  width: 730,
+  height: 600,
   scene: {
       preload: preload,
       create: create,
@@ -23,7 +23,7 @@ function preload ()
     this.load.image('goal', 'assets/goal.png');
 }
 
-let baseXCoordinate = 300; //change value to 100?
+let baseXCoordinate = 160;
 let xSpacing = 100; //will be image height of the gate
 let baseYCoordinate = 100; 
 let ySpacing = 100; //will be image width of the gate
@@ -80,53 +80,52 @@ function create ()
     this.add.image(xCoordinateGrid(3), yCoordianteGrid(4), 'zgate');
     this.add.image(xCoordinateGrid(4), yCoordianteGrid(4), 'hgate');
 
-    this.add.image(750, 500, 'goal');
-
     this.add.image(xCoordinateHorizontalWall(0), yCoordinateHorizontalWall(0), 'wallh');
     this.add.image(xCoordinateHorizontalWall(1), yCoordinateHorizontalWall(0), 'wallh');
     this.add.image(xCoordinateHorizontalWall(2), yCoordinateHorizontalWall(0), 'wallh');
     this.add.image(xCoordinateHorizontalWall(3), yCoordinateHorizontalWall(0), 'wallh');
     this.add.image(xCoordinateHorizontalWall(4), yCoordinateHorizontalWall(0), 'wallh');
-
+    
     this.add.image(xCoordinateHorizontalWall(1), yCoordinateHorizontalWall(1), 'wallh');
     this.add.image(xCoordinateHorizontalWall(3), yCoordinateHorizontalWall(1), 'wallh');
-
+    
     this.add.image(xCoordinateHorizontalWall(2), yCoordinateHorizontalWall(2), 'wallh');
     this.add.image(xCoordinateHorizontalWall(3), yCoordinateHorizontalWall(2), 'wallh');
-
+    
     this.add.image(xCoordinateHorizontalWall(2), yCoordinateHorizontalWall(4), 'wallh');
     this.add.image(xCoordinateHorizontalWall(3), yCoordinateHorizontalWall(4), 'wallh');
     this.add.image(xCoordinateHorizontalWall(4), yCoordinateHorizontalWall(4), 'wallh');
-
+    
     this.add.image(xCoordinateHorizontalWall(0), yCoordinateHorizontalWall(5), 'wallh');
     this.add.image(xCoordinateHorizontalWall(1), yCoordinateHorizontalWall(5), 'wallh');
     this.add.image(xCoordinateHorizontalWall(2), yCoordinateHorizontalWall(5), 'wallh');
     this.add.image(xCoordinateHorizontalWall(3), yCoordinateHorizontalWall(5), 'wallh');
     this.add.image(xCoordinateHorizontalWall(4), yCoordinateHorizontalWall(5), 'wallh');
-
+    
     this.add.image(xCoordinateVertWall(0), yCoordinateVertWall(1), 'wallv');
     this.add.image(xCoordinateVertWall(0), yCoordinateVertWall(2), 'wallv');
     this.add.image(xCoordinateVertWall(0), yCoordinateVertWall(3), 'wallv');
     this.add.image(xCoordinateVertWall(0), yCoordinateVertWall(4), 'wallv');
-
+    
     this.add.image(xCoordinateVertWall(1), yCoordinateVertWall(1), 'wallv');
     this.add.image(xCoordinateVertWall(1), yCoordinateVertWall(2), 'wallv');
     this.add.image(xCoordinateVertWall(1), yCoordinateVertWall(3), 'wallv');
     this.add.image(xCoordinateVertWall(1), yCoordinateVertWall(4), 'wallv');
-
+    
     this.add.image(xCoordinateVertWall(2), yCoordinateVertWall(3), 'wallv');
-
+    
     this.add.image(xCoordinateVertWall(4), yCoordinateVertWall(2), 'wallv');
     this.add.image(xCoordinateVertWall(4), yCoordinateVertWall(3), 'wallv');
-
+    
     this.add.image(xCoordinateVertWall(5), yCoordinateVertWall(0), 'wallv');
     this.add.image(xCoordinateVertWall(5), yCoordinateVertWall(1), 'wallv');
     this.add.image(xCoordinateVertWall(5), yCoordinateVertWall(2), 'wallv');
     this.add.image(xCoordinateVertWall(5), yCoordinateVertWall(3), 'wallv');
-
-    this.add.image(200, 100, 'qbitup');
-    this.add.image(820, 500, 'solutionleft');
-}
+    
+    this.add.image(xCoordinateGrid(-1), yCoordianteGrid(0), 'qbitup');
+    this.add.image(xCoordinateGrid(5.1), yCoordianteGrid(4), 'solutionleft');
+    this.add.image(xCoordinateGrid(4.5), yCoordianteGrid(4), 'goal');
+  }
 
 function update ()
 {
