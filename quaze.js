614adc7484accfcbd66a2b14db15308175e52582
Vatsal Kanoria <vatskan@gmail.com>
2021-10-02 21:11:18 +0200

    add rotation of qubit with bugs

--- quaze.js
+++ quaze.js
@@ -19,6 +19,9 @@ let config = {
 let game = new Phaser.Game(config);
 let qubit;
 let walls;
+let hgates;
+let xgates;
+let zgates;
 
 function preload ()
 {
@@ -55,8 +58,11 @@ const gameData = {
   }, //the grid is 5x5
   gatesGrid: [[h, x, z, h, x], [z, h, x, x, h], [h, z, h, x, z], [z, x, z, h, h], [x, h, x, z, h]], //each subarray represents a row starting from the top of the grid
   horizontalWalls: [[T, T, T, T, T], [F, T, F, T, F], [F, F, T, T, F], [F, F, F, F, F], [F, F, T, T, T], [T, T, T, T, T]], //each subarray represents a row starting from the top of the grid
-  verticalWalls: [[F, T, T, T, T], [F, T, T, T, T], [F, F, F, T, F], [F, F, F, F, F], [F, F, T, T, F], [T, T, T, T, F]] //each subarray represents a column starting from the left of the grid
+  verticalWalls: [[F, T, T, T, T], [F, T, T, T, T], [F, F, F, T, F], [F, F, F, F, F], [F, F, T, T, F], [T, T, T, T, F]], //each subarray represents a column starting from the left of the grid
+  qubitInitialState: 0, //facing up
+  qubitFinalState: 3, //facing left
 }
+let qubitState = gameData.qubitInitialState; //initial state of qubit is facing up. (0 = up, 1 = right, 2 = down, 3 = left)
 
 function create ()
 {
@@ -91,7 +97,7 @@ function create ()
     }
   }
 
-  this.add.image(xCoordinateGrid(4.5), yCoordianteGrid(4), 'goal');
+  let goal = this.physics.add.image(xCoordinateGrid(4.5), yCoordianteGrid(4), 'goal');
   this.add.image(xCoordinateGrid(5)+10, yCoordianteGrid(4), 'solutionleft');
   
   qubit = this.physics.add.image(xCoordinateGrid(-1), yCoordianteGrid(0), 'qbitup');
@@ -99,22 +105,64 @@ function create ()
   
   this.physics.add.collider(qubit, walls); 
 
+  let qubitAngles = {
+    0: 0, //up
+    1: 90, //right
+    2: 180, //down
+    3: -90 //left
+  }
+  let overlap = ''
   function hTransform(qubit, gate){
-    console.log('qubit', qubit)
-    console.log('hgate', gate)
+    gate.disableBody(true, true); //does not work if you go back on the gate
+    if (qubitState===0 && overlap!=='hgate') {
+      qubitState = 1
+      qubit.angle = qubitAngles[qubitState];
+    }else if (qubitState===1 && overlap!=='hgate') {
+      qubitState = 0
+      qubit.angle = qubitAngles[qubitState];
+    }else if (qubitState===2 && overlap!=='hgate') {
+      qubitState = 3
+      qubit.angle = qubitAngles[qubitState];
+    }else if (qubitState===3 && overlap!=='hgate') {
+      qubitState = 2
+      qubit.angle = qubitAngles[qubitState];
+    }
+    overlap = 'hgate' //BUG: this overlap solution will not work if two Hgates are next to each other on the grid!!!!
   }
   function xTransform(qubit, gate){
-    console.log('qubit', qubit)
-    console.log('xgate', gate)
+    gate.disableBody(true, true);
+    if(qubitState===0 && overlap!=='xgate'){
+      qubitState = 2
+      qubit.angle = qubitAngles[qubitState];
+	  }else if(qubitState===2 && overlap!=='xgate'){
+      qubitState = 0
+      qubit.angle = qubitAngles[qubitState];
+    }
+    overlap = 'xgate' //BUG: this overlap solution will not work if two Xgates are next to each other on the grid!!!!
   }
   function zTransform(qubit, gate){
-    console.log('qubit', qubit)
-    console.log('zgate', gate)
+    gate.disableBody(true, true);
+    if(qubitState===1 && overlap!=='zgate'){
+      qubitState = 3
+      qubit.angle = qubitAngles[qubitState];
+    }else if(qubitState===3 && overlap!=='zgate'){
+      qubitState = 1
+      qubit.angle = qubitAngles[qubitState];
+    }
+    overlap='zgate' //BUG: this overlap solution will not work if two Zgates are next to each other on the grid!!!!
+  }
+  function winOrLose(qubit, goal){
+    if(qubitState===gameData.qubitFinalState){
+      console.log('you win') //change to display text on game
+    }else{
+      console.log('you lose') //change to display text on game
+    }
   }
 
   this.physics.add.overlap(qubit, hgates, hTransform, null, this); 
   this.physics.add.overlap(qubit, xgates, xTransform, null, this); 
   this.physics.add.overlap(qubit, zgates, zTransform, null, this); 
+  this.physics.add.overlap(qubit, goal, winOrLose, null, this); 
 
   cursors = this.input.keyboard.createCursorKeys(); 
 }
